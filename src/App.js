566ad2094ffb2c792dc5c5218b87739b9f13d863
Your Name <you@example.com>
2021-12-07 19:55:24 -0500

    estableciendo variables de entorno

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import UserRegistration from './components/UserRegistration';
 import Parse from 'parse/dist/parse.min.js';
 import Login from './components/Login';
 // Your Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = 'hK7IIc9YvZYSNYkmMbW70D3tHxGoJPEJwaAvSBU6';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'uNqd4stEkA6z5M4hBAw6YAQy3dTEToTUWlD5vcF2';
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
+
+Parse.initialize(process.env.REACT_APP_PARSE_APPLICATION_ID, process.env.REACT_APP_PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = process.env.REACT_APP_PARSE_HOST_URL;
 
 
 
